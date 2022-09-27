1b271d40ca98b1f644df6439409798dedcc084ac
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-09-27 12:42:55 -0400

    part 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -10,6 +10,13 @@
             int remainder = a % b;
             if (a==17&&b==4)
             Console.WriteLine($"{a}/{b} is {quotient} with the remainder {remainder}");
+            Console.WriteLine("whats the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double Area = Math.PI * radius * radius;
+            Console.WriteLine($"The area of a circle with the radius of {radius} is {Area}" );
+
         }
+       
+
     }
 }
